4162161bdf4a007cd0f597b903873b38d1cbc5e7
Steve Burke <stevepburke@hotmail.com>
2012-03-15 11:13:52 -0600

    This will be my initial commit of my first project.
    As done via http://ruby.railstutorial.org/ruby-on-rails-tutorial-book#sec:adding_and_committing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f4379a2578f73091caf1aa0c4c1e456284a410ea0ab6cf8894bcd6f5274a6d37de171c06bebc4fdfa71d717824ab2f79b68f2f2eb77e55cfd21d31b42334801e'
